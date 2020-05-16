68c6f034038760c197ca02bc4c4b2ac4351d77fb
titusefferian <titus.efferian@tokopedia.com>
2020-05-16 15:25:45 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+const App = () => {
+    return (
+        <h1>hello world, react-app by: titusefferian</h1>
+    )
+}
+
+export default hot(module)(App);
